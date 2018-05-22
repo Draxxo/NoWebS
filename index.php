653dedfd947fe0820854ef57d959254cf6da8865
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-05-22 10:24:37 +0200

    Commit with index & script

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>First cours</title>
+		<link rel="stylesheet" type="text/css" href="script.js">
+	</head>
+	<body>
+		<h1>Hello World !</h1>
+	</body>
+</html>
