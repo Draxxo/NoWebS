0dc9d477c56d839321e0540b557939312b5d6d96
Alexis Prevost-Maynen <killer76520@gmail.com>
2018-05-22 20:00:27 +0200

    Modification de la stucture
    - Amélioration de l'algo qui calcul le taux mensuel
    - Création d'un objet "comptable" qui va permettre de stoker les variables et fonction associé

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 /* Formulaire - Simulateur de prêt */
 
+//Comptable qui va faire les comptes !
+var comptableOfForm;
+function comptable(montant, duree, taux) {
+	this.montant = montant;
+	this.duree   = duree;
+	this.taux    = taux;
+}
+//Calcul du taux d'intêret 
+comptable.prototype.calculTauxInteret = function() {
+	var resultat = "Erreur";
+
+	var tauxMensuel = this.taux/100/12;
+	resultat = (this.montant * (tauxMensuel)) / (1 - Math.pow(1+tauxMensuel, this.duree*-12)); //duree en mois
+
+	return resultat.toFixed(2);
+}
+
 var button = document.getElementById('submit');
 
-// Ce déclenche au click du button
+// Ce déclenche au click du button du formulaire
 button.onclick = function() {
 
 	var data = serializeForm(); //On serialize
 
-    var resultat = calculTauxInteret(data['montant'], data['duree'], data['taux']); // On calcul le taux
+	comptableOfForm = new comptable(data['montant'], data['duree'], data['taux']);
 
-    document.getElementById('resultat').innerHTML = '=> ' + resultat; // On affiche le résultat
+    var resultat = comptableOfForm.calculTauxInteret() // On calcul le taux
+
+    document.getElementById('resultat').innerHTML = '=> ' + resultat + ' par mois pendant ' + data['duree'] + ' an(s)'; // On affiche le résultat
 
     return false;
 };
 
-//Calcul du taux d'intêret 
-function calculTauxInteret(montant, duree, taux) {
-	var resultat = "Erreur";
-
-	var tauxMensuel = taux/100/12;
-	resultat = (montant * (tauxMensuel)) / (1 - Math.pow(1+tauxMensuel, duree*12)); //duree en mois
-
-	return resultat;
-}
-
-//Serialization de mon formulaire
+//Serialization du formulaire
 function serializeForm() {
 	var data = [];
 	var inputs = document.getElementsByTagName("input"); 
